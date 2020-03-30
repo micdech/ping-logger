80075e6056116e154b086be8e967c095537ad276
micdech <micdech@gmail.com>
2020-03-30 16:03:43 +0200

    evoluzione del repo forkato

--- ping-script.sh
+++ ping-script.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-
 data=`date`
+emails="micdech@gmail.com"
+subject="invio mail automatica from pinger-tools"
 # Definisco host da testare nel ciclo for
 
-for i in 8.8.8.8 8.8.4.4 1.1.1.1
+# può essere anche array separato da spazi (es: www.ilcrivello.it www.google.com etc)
+for i in www.ilcrivello.dec
 do
+echo "$data Host $i test start" >> /var/log/ping-logger.log
 
 ((count = 10))                            # Maximum number to try.
 while [[ $count -ne 0 ]] ; do
-    ping -c 1 $i > /dev/null                     # Try once.
+    ping -c 1 $i >> /var/log/ping-logger.log # Try once.
     rc=$?
     if [[ $rc -eq 0 ]] ; then
         ((count = 1))                      # If okay, flag to exit loop.
@@ -17,9 +20,24 @@ while [[ $count -ne 0 ]] ; do
 done
 
 if [[ $rc -eq 0 ]] ; then                  # Make final determination.
-    echo "$data Host $i is up." > /dev/null
+    echo "$data Host $i is up." >> /var/log/ping-logger.log
 else
     echo "$data Host $i is unreachable." >> /var/log/ping-logger.log
+    if (( $(ps -ef | grep -v grep | grep apache2 | wc -l) > 0 ))
+		then
+			echo "$data Apache webserver is running, try to restart HAproxy Varnish && Apache" >> /var/log/ping-logger.log
+			echo `service apache2 restart`
+			echo `service varnish restart`
+			echo `service haproxy restart`
+			echo "Apache, Varnish & HAProxy for $i was stopped and now are started!!!" | mail -s "$subject" $emails
+	else
+			echo "$data Apache webserver is running, try to restart HAproxy Varnish && Apache" >> /var/log/ping-logger.log
+			echo `service apache2 start`
+			echo `service varnish start`
+			echo `service haproxy start`
+			echo "Apache, Varnish & HAProxy for $i are now restarted due possible choke!!!" | mail -s "$subject" $emails
+
+	fi
 fi
 
 done
